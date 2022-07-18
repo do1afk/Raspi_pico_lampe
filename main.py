7d5201f98d57790385ce8805192dede74d29f397
Arndt Karger <76599718+do1afk@users.noreply.github.com>
2022-07-18 18:52:47 +0200

    Hysterese eingepflegt

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ rot    = Pin(13, Pin.OUT)           #GPIO Pin für rot
 
 ####################################################################
 #Variablen:
-minBatVoltage = 1000                 #Batteirespannung wenn alles ok ist
+TempHyst = 0                         #Hystereselvl für Temperatur
+SpgHyst = 0                          #Hystereselvl für Spannung
+minBatVoltage = 10000                 #Batteirespannung wenn alles ok ist
 maxTemp = 30000                      #maximale Temperatur wenn alles ok ist
 
 ####################################################################
@@ -60,10 +62,11 @@ def writeState(state):             #schreibe etnsprechenden Modus
 def readSpg():               #lese GPIO26 aus
     read = spg26.read_u16()
     #print("Spannung: ", read)
-    #sleep(0.01)             #aus irgendwelchen Gründen lies er sonst nur Müll
+    sleep(0.01)             #aus irgendwelchen Gründen lies er sonst nur Müll
     return read
 
 def writeBat(read):         #schreibe Batteriezustand
+    print("Schreibe")
     if read > 30000:
        gruen0.on()
        gruen1.on()
@@ -87,12 +90,16 @@ def writeBat(read):         #schreibe Batteriezustand
     return 0
 
 def batDown():                     #Notprogramm wenn Batterie leer ist
-    pwm14.duty(0)             #dann schalte lampe aus
+    pwm14.duty(0)                  #dann schalte lampe aus
     
+    gruen0.off()                   #alle leds ausschalten
+    gruen1.off()
+    gelb.off()
+    rot.off()
     
-    rot.off()                 #blinken
+    rot.on()                      #blinken
     sleep(0.5)
-    rot.on()
+    rot.off()
     sleep(0.5)
     return 0
 
@@ -118,35 +125,50 @@ def toHot():                      #wenn Platinentemp zu hoch wird
         rot.on()
         
         sleep(0.5)
+        
+        
        
         
 
 ####################################################################
-#main:        
+#main:
 while True:
-    if readSpg() > minBatVoltage:      #benötige Spannung abfragen um blinken zu verhindern
+    print("gre : ", minBatVoltage)
+    print("akt : ", readSpg())
+    #sleep(0.5)
+#1. Hysteresen
+#Temperatur:
+    if readTemp() > maxTemp and TempHyst == 0:         #wenn unteres Hystereselevel und maxtemp überschritten wurde:
+        maxTemp = 20000                                #neue maxtemp setzten
+        TempHyst = 1                                   #neues Hystereselvl setzten
+        
+    if readTemp() < maxTemp and TempHyst == 1:         #wenn oberes Hysterelevel und maxtemp unterschritten:
+        maxTemp = 30000                                #neue maxtemp setzten
+        TempHyst = 0                                   #neues Hystereselvl setzten
+    
+    #Spannung:  
+    if readSpg() < minBatVoltage and SpgHyst == 0:    #wenn unteres Hystereselevel und mindest SPG unterschritten wurde:
+        minBatVoltage = 15000                         #neue mindest SPG setzten
+        SpgHyst = 1                                   #neues Hystereselvl setzten
+        
+    if readSpg() > minBatVoltage and SpgHyst == 1:    #wenn oberes Hysterelevel und mindest SPG überschritten:
+        minBatVoltage = 10000                         #neue mindest SPG setzten
+        SpgHyst = 0                                   #neues Hystereselvl setzten
+
+#2. Bedierwarungen ausgeben:       
+    if readTemp() > maxTemp:
+        toHot()                                        #Temperaturwarnung ausgeben
+    
+    if readSpg() < minBatVoltage:
+        batDown()                                     #Spannungswarung ausgeben
+
+#3. Nur wenn alles ok ist, Licht anschalten und Batteriezustand anzeigen:
+    if readSpg() > minBatVoltage and readTemp() < maxTemp:      #benötige Spannung abfragen um blinken zu verhindern
         writeState(getState())        #schreibe den Modus, den das Wahlrad vorgibt
-        
-    writeBat(readSpg())            #schreibe den gelesenen Batteriezustand
-    
-    print("Max temp: ", maxTemp)
-    print("akt temp: ", readTemp())
-    
-    if readSpg() < minBatVoltage:   #fällt die Batteriespannung unter kritischen wert
-        minBatVoltage = 15000     #erhöhe mindestspannung
-        batDown()                 #rot blinken, kritischen Wert erhöhen und Licht aus
+        writeBat(readSpg())            #schreibe den gelesenen Batteriezustand
+        #Hier ist noch ein BUG!
         
 
-    
-    if readTemp() > maxTemp:       #wenn Platinentemp zu hoch wird
-        maxTemp = 20000           #maximal Temperatur erniedirgen
-        toHot()                  #und gib gescheid
-    
-
-
-    
-  
-    
     
     
     
